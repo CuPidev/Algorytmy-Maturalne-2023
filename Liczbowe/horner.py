286e8b2652e9f9e8f1654b482512f165ff3994c9
CuPidev <m41hmc@gmail.com>
2022-02-06 21:52:43 +0100

    Update horner.py

--- Liczbowe/horner.py
+++ Liczbowe/horner.py
@@ -3,3 +3,10 @@ def horner(wspolczynniki, ilosc_wspolczynnikow, argument):
     for i in range(1, ilosc_wspolczynnikow):
         wynik = wynik * argument + wspolczynniki[i]
     return wynik
+
+
+wsp = [2, 4, 5, 6]
+x = 3
+n = len(wsp)
+
+print(horner(wsp, n, x))
